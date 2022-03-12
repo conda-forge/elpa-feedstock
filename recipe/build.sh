cf6f9a40b9a2b498d21a8b61299e856c5898df4e
Isuru Fernando <isuruf@gmail.com>
2022-03-12 11:31:05 -0600

    set OPAL_PREFIX and don't use full path

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,10 @@ tests=(
 if [ "${mpi}" != "nompi" ]; then
   MPI=yes
   SUFFIX=""
-  export CXX="$PREFIX/bin/mpicxx" CC="$PREFIX/bin/mpicc" FC="$PREFIX/bin/mpifort"
+  export CXX=mpicxx
+  export CC=mpicc
+  export FC=mpifort
+  export OPAL_PREFIX="$PREFIX"
 else
   MPI=no
   SUFFIX="_onenode"
