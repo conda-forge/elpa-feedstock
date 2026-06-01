9b2615460e96fc263028ef4634ec0e2073eb186c
Matthias Krack <matthias.krack@psi.ch>
2026-06-01 15:58:25 +0200

    Add further fixes for cross-compilation
    MNT: Re-rendered with conda-smithy 2026.5.29 and conda-forge-pinning 2026.06.01.01.47.52

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -ex
 # Update config.guess / config.sub
 cp "${BUILD_PREFIX}/share/gnuconfig/config."* .
 
-# Fix hardcoded perl interpreter (and remove -w which breaks env)
+# Fix perl shebangs (portable)
 find . -name "*.pl" -exec sed -i.bak '1s|^#!.*perl.*|#!/usr/bin/env perl|' {} +
 find . -name "*.pl.bak" -delete
 
@@ -13,42 +13,51 @@ tests=(
   "validate_real_2stage_banded_default.sh"
 )
 
-# MPI setup
+# MPI setup (CRITICAL: avoid wrappers during cross-compilation)
 if [[ "${mpi}" != "nompi" ]]; then
   MPI=yes
+
   export OMPI_CC="${CC}"
   export OMPI_CXX="${CXX}"
   export OMPI_FC="${FC}"
   export MPICH_CC="${CC}"
   export MPICH_CXX="${CXX}"
   export MPICH_FC="${FC}"
-  export CC="${PREFIX}/bin/mpicc"
-  export CXX="${PREFIX}/bin/mpicxx"
-  export FC="${PREFIX}/bin/mpifort"
+
+  # Only use MPI wrappers for native builds
+  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
+    export CC="${PREFIX}/bin/mpicc"
+    export CXX="${PREFIX}/bin/mpicxx"
+    export FC="${PREFIX}/bin/mpifort"
+  fi
 else
   MPI=no
 fi
 
-# OpenMPI CI stabilization
+# OpenMPI CI stabilization (runtime only)
 if [[ "${mpi}" == "openmpi" ]]; then
   export OMPI_MCA_plm=isolated
   export OMPI_MCA_btl_vader_single_copy_mechanism=none
   export OMPI_MCA_rmaps_base_oversubscribe=yes
 fi
 
-# Compiler and architecture flags
+# Compiler + architecture flags
 conf_extra=""
+
 if [[ "$(uname)" == "Darwin" ]]; then
   if [[ "${target_platform}" == "osx-arm64" ]]; then
+    # LTO problematic in cross builds
     export CFLAGS="${CFLAGS} -fno-lto"
     export FCFLAGS="${FCFLAGS} -fno-lto"
     export CXXFLAGS="${CXXFLAGS} -fno-lto"
+
     conf_extra="--disable-sse-assembly --disable-avx2 --disable-avx --disable-sse"
   else
     export CFLAGS="${CFLAGS} -mavx"
     export FFLAGS="${FFLAGS} -mavx"
     conf_extra="--disable-sse-assembly --disable-avx2"
   fi
+
   export FORTRAN_CPP="${FC:-gfortran} -E -P -cpp"
 else
   if [[ "${target_platform}" == "linux-64" ]]; then
@@ -57,10 +66,20 @@ else
   else
     conf_extra="--disable-sse-assembly --disable-avx2 --disable-avx --disable-sse"
   fi
+
   export FORTRAN_CPP="${CPP:-cpp} -P -traditional"
 fi
 
-# Ensure PREFIX paths are visible
+# Cross-compilation fixes
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  export ac_cv_prog_cxx_works=yes
+  export ac_cv_prog_cc_works=yes
+  export ac_cv_prog_fc_works=yes
+  export ac_cv_prog_cxx_cross=yes
+  export cross_compiling=yes
+fi
+
+# Ensure PREFIX visibility
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export FCFLAGS="${FCFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
@@ -69,11 +88,12 @@ if [[ "${MPI}" == "yes" ]]; then
   export LIBS="${LIBS} -lscalapack"
 fi
 
-# Configure arguments
+# Configure options
 conf_options=(
   "--prefix=${PREFIX}"
   "--build=${BUILD}"
   "--host=${HOST}"
+  "--target=${HOST}"
   "--with-mpi=${MPI}"
   "--disable-avx512"
 )
@@ -82,24 +102,29 @@ if [[ -n "${conf_extra}" ]]; then
   conf_options+=(${conf_extra})
 fi
 
-# Build without OpenMP
+# Build (no OpenMP)
 mkdir build
 pushd build
 
-../configure "${conf_options[@]}" || { cat config.log; exit 1; }
+../configure "${conf_options[@]}" \
+  CC="${CC}" CXX="${CXX}" FC="${FC}" \
+  || { cat config.log; exit 1; }
+
 make -j"${CPU_COUNT:-1}"
 make install
 
 popd
 
-# Build with OpenMP
+# Build (with OpenMP)
 mkdir build_openmp
 pushd build_openmp
 
-../configure --enable-openmp "${conf_options[@]}"
+../configure --enable-openmp "${conf_options[@]}" \
+  CC="${CC}" CXX="${CXX}" FC="${FC}"
+
 make -j"${CPU_COUNT:-1}"
 
-# Run minimal tests (only when not cross-compiling)
+# Run tests only if safe
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" && "${MPI}" == "yes" ]]; then
   for t in "${tests[@]}"; do
     make "$t" && ./"$t" || true
@@ -109,3 +134,4 @@ fi
 make install
 
 popd
+
